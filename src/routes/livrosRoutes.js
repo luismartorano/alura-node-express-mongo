8d5ae43d2debcc8c93b9e75c6490d5449984b4db
Luis Guilherme Martorano <luis.tavares@vitta.me>
2023-08-22 11:05:30 -0300

    aula5-paginação

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import LivroController from '../controllers/livrosController.js';
+import paginar from '../middlewares/paginar.js';
 
 const router = express.Router();
 
 router
-  .get('/livros', LivroController.listarLivros)
-  .get('/livros/busca', LivroController.listarLivroPorFiltro) // da especifica para a menos específica, senão vai dar erro. Busca é mais especifica que :id
+  .get('/livros', LivroController.listarLivros, paginar)
+  .get('/livros/busca', LivroController.listarLivroPorFiltro, paginar)
   .get('/livros/:id', LivroController.listarLivroPorId)
   .post('/livros', LivroController.cadastrarLivro)
   .put('/livros/:id', LivroController.atualizarLivro)
